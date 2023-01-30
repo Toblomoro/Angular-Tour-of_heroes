203bfdab889b2a517e44a1a3ad4b63925a7206b7
Ben Voges <108232180+Toblomoro@users.noreply.github.com>
2023-01-30 14:25:58 -0600

    Update Hero functions and functioning

--- angular-tour-of-heroes/src/app/hero.service.ts
+++ angular-tour-of-heroes/src/app/hero.service.ts
@@ -40,12 +40,12 @@ getHeroes(): Observable<Hero[]> {
     );
 }
 /** GET hero by id. Will 404 if id not found */
-getHero(id:number): Observable<Hero>{
-  const url = '${this.heroesUrl}/${id}';
+getHero(id: number): Observable<Hero> {
+  const url = `${this.heroesUrl}/${id}`;
   return this.http.get<Hero>(url).pipe(
-    tap(_ => this.log('fetched hero id = ${id}')),
-    catchError(this.handleError<Hero>('getHero id=${id}'))
-  )
+    tap(_ => this.log(`fetched hero id=${id}`)),
+    catchError(this.handleError<Hero>(`getHero id=${id}`))
+  );
 }
 
 
@@ -59,4 +59,15 @@ getHero(id:number): Observable<Hero>{
       this.messageService.add(`HeroService: ${message}`);
       
     }
+    httpOptions = {
+      headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+    };
+
+    /** PUT: update the hero on the server */
+updateHero(hero: Hero): Observable<any> {
+  return this.http.put(this.heroesUrl, hero, this.httpOptions).pipe(
+    tap(_ => this.log(`updated hero id=${hero.id}`)),
+    catchError(this.handleError<any>('updateHero'))
+  );
+}
 }
